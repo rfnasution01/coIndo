481cc6ec5d464f20265b9d05e6ffe8e24763a2aa
rfnasution01 <rivaifnasution@gmail.com>
2024-03-31 15:27:04 +0700

    feat: integrasi exchange

--- src/feature/markets/market-info.tsx
+++ src/feature/markets/market-info.tsx
@@ -1,91 +1,18 @@
-import { Badge } from '@/components/Badge'
-import { CardHelper, CardMapping } from '@/components/ui/card'
-import { FormatManipulationComponent } from '@/lib/helpers/formatComponent'
-import { calculateCurrency, roundToNDecimals } from '@/lib/helpers/formatNumber'
-import { capitalizeFirstLetterFromLowercase } from '@/lib/helpers/formatText'
-import { MarketsType } from '@/lib/interfaces/marketsProps'
-import { getCurrencySlice } from '@/store/reducer/stateCurrency'
-import { useSelector } from 'react-redux'
-
+import { ReactNode } from 'react'
 export function MarketInfo({
-  sortedByPercent,
-  sortedByTrades,
-  sortedByVolume,
+  card1,
+  card2,
+  card3,
 }: {
-  sortedByVolume: MarketsType[]
-  sortedByPercent: MarketsType[]
-  sortedByTrades: MarketsType[]
+  card1?: ReactNode
+  card2?: ReactNode
+  card3?: ReactNode
 }) {
-  const stateCurrency = useSelector(getCurrencySlice)
-
   return (
     <div className="grid grid-cols-12 gap-32 phones:hidden">
-      {/* --- Top Volume --- */}
-      <CardHelper
-        title="Top Volume 24Hr"
-        mappingData={
-          <div className="flex flex-col gap-y-16">
-            {sortedByVolume?.slice(0, 5).map((item, idx) => (
-              <CardMapping
-                idx={idx}
-                label={capitalizeFirstLetterFromLowercase(item?.exchangeId)}
-                description={`${item?.baseSymbol}/${item?.quoteSymbol}`}
-                subLabel={
-                  <FormatManipulationComponent
-                    currencySymbol={stateCurrency?.currencySymbol}
-                    price={calculateCurrency({
-                      cryptoCurrency: Number(item?.volumeUsd24Hr),
-                      fiatCurrency: stateCurrency?.price,
-                      jumlah: 1,
-                    })}
-                  />
-                }
-              />
-            ))}
-          </div>
-        }
-      />
-      {/* --- Top Percentage --- */}
-      <CardHelper
-        title="Top Kenaikan 24Hr"
-        mappingData={
-          <div className="flex flex-col gap-y-16">
-            {sortedByPercent
-              ?.slice(0, 5)
-              .map((item, idx) => (
-                <CardMapping
-                  idx={idx}
-                  label={capitalizeFirstLetterFromLowercase(item?.exchangeId)}
-                  description={`${item?.baseSymbol}/${item?.quoteSymbol}`}
-                  subLabel={
-                    <Badge variant="success">
-                      +
-                      {roundToNDecimals(Number(item?.percentExchangeVolume), 2)}
-                    </Badge>
-                  }
-                />
-              ))}
-          </div>
-        }
-      />
-      {/* --- Top Trade --- */}
-      <CardHelper
-        title="Top Trade 24Hr"
-        mappingData={
-          <div className="flex flex-col gap-y-16">
-            {sortedByTrades
-              ?.slice(0, 5)
-              .map((item, idx) => (
-                <CardMapping
-                  idx={idx}
-                  label={capitalizeFirstLetterFromLowercase(item?.exchangeId)}
-                  description={`${item?.baseSymbol}/${item?.quoteSymbol}`}
-                  subLabel={item?.tradesCount24Hr}
-                />
-              ))}
-          </div>
-        }
-      />
+      {card1}
+      {card2}
+      {card3}
     </div>
   )
 }
--- src/pages/exchange/index.tsx
+++ src/pages/exchange/index.tsx
@@ -1,3 +1,96 @@
+import { Badge } from '@/components/Badge'
+import { Table } from '@/components/Table'
+import { CardHelper, CardMapping } from '@/components/ui/card'
+import { ContentTemplate } from '@/feature/main-layout/content-layout/content-template'
+import { MarketInfo } from '@/feature/markets'
+import { columnsExchanges } from '@/lib/consts/dummy/Tables'
+import { roundToNDecimals } from '@/lib/helpers/formatNumber'
+import { capitalizeFirstLetterFromLowercase } from '@/lib/helpers/formatText'
+import { useSearch } from '@/lib/hooks/useSearch'
+import { ExchangeType } from '@/lib/interfaces/exchangesProps'
+import { useGetExchangesQuery } from '@/store/slices/exchangesAPI'
+import { Link } from 'lucide-react'
+import { useEffect, useState } from 'react'
+
 export default function Exchange() {
-  return <h1>Test</h1>
+  const { currentPage } = useSearch()
+
+  const { data, isLoading, isFetching } = useGetExchangesQuery()
+  const [dataExchanges, setDataExchanges] = useState<ExchangeType[]>()
+
+  useEffect(() => {
+    if (data?.data) {
+      setDataExchanges(data?.data)
+    }
+  }, [data])
+
+  const sortedMarket = dataExchanges ? [...dataExchanges] : []
+
+  const sortedByPercent = sortedMarket.sort((a, b) => {
+    return parseFloat(b.percentTotalVolume) - parseFloat(a.percentTotalVolume)
+  })
+
+  const dataPerPage = 100
+  const totalData = dataExchanges ? dataExchanges.length : 0
+  const totalPage = Math.ceil(totalData / dataPerPage)
+
+  return (
+    <ContentTemplate
+      totalPage={totalPage}
+      table={
+        <Table
+          data={
+            dataExchanges?.slice(
+              currentPage * dataPerPage - dataPerPage,
+              currentPage * dataPerPage,
+            ) ?? []
+          }
+          columns={columnsExchanges}
+          containerClasses="h-full phones:w-[calc(100vw_-_11rem)]"
+          loading={isLoading || isFetching}
+        />
+      }
+      infoComponent={
+        <MarketInfo
+          card1={
+            <CardHelper
+              title="Popular Exchange"
+              mappingData={
+                <div className="flex flex-col gap-y-16">
+                  {sortedByPercent?.slice(0, 5).map((item, idx) => (
+                    <CardMapping
+                      idx={idx}
+                      label={capitalizeFirstLetterFromLowercase(
+                        item?.exchangeId,
+                      )}
+                      description={
+                        <a
+                          href={item?.exchangeUrl}
+                          target="_blank"
+                          className="flex items-center gap-x-8"
+                        >
+                          <Link size={12} />
+                          <span>{item?.tradingPairs} pairs</span>
+                        </a>
+                      }
+                      subLabel={
+                        <Badge variant="success">
+                          +
+                          {roundToNDecimals(
+                            Number(item?.percentTotalVolume),
+                            2,
+                          )}
+                        </Badge>
+                      }
+                    />
+                  ))}
+                </div>
+              }
+            />
+          }
+          card2={<CardHelper title="Exchange News" />}
+        />
+      }
+    />
+  )
 }
